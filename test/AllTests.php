4ac5246f740f6fab275ffdda4bb6caa356f70678
xiezefan <xiezefan@126.com>
2014-06-11 16:11:34 +0800

    add new version test

--- test/AllTests.php
+++ test/AllTests.php
@@ -37,6 +37,7 @@ class AllTests {
 
 
 
+
         return $suite;
     }
 } 
---
+++ test/TestAll.php
@@ -0,0 +1,18 @@
+<?php
+include_once '../src/JPush/Model/Audience.php';
+include_once '../src/JPush/Model/Message.php';
+include_once '../src/JPush/Model/Notification.php';
+include_once '../src/JPush/Model/Options.php';
+include_once '../src/JPush/Model/Platform.php';
+include_once '../src/JPush/Model/PushPayload.php';
+include_once '../src/JPush/Model/PushResponse.php';
+include_once '../src/JPush/Model/ReportResponse.php';
+include_once '../src/JPush/Model/Report.php';
+include_once '../src/JPush/JPushClient.php';
+
+use JPush\Model as M;
+use JPush\JPushClient;
+
+class TestAll {
+
+} 
