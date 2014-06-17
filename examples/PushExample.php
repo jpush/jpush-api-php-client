22864c93e8c0081d6780023228c664c22eb539eb
xiezefan <xiezefan@126.com>
2014-06-17 15:23:10 +0800

    add  example composer support

--- examples/PushExample.php
+++ examples/PushExample.php
@@ -8,6 +8,9 @@ use JPush\JPushLog;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
+use JPush\Exception\APIConnectionException;
+use JPush\Exception\APIRequestException;
+
 $br = '<br/>';
 $spilt = ' - ';
 
@@ -20,64 +23,91 @@ JPushLog::setLogHandlers(array(new StreamHandler('jpush.log', Logger::DEBUG)));
 $client = new JPushClient($app_key, $master_secret);
 
 //easy push
-$result = $client->push()
-    ->setPlatform(M\all)
-    ->setAudience(M\all)
-    ->setNotification(M\notification('Hi, JPush'))
-    ->printJSON()
-    ->send();
-
-if ($result->ok) {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
+try {
+    $result = $client->push()
+        ->setPlatform(M\all)
+        ->setAudience(M\all)
+        ->setNotification(M\notification('Hi, JPush'))
+        ->printJSON()
+        ->send();
+    echo 'Push Success.' . $br;
     echo 'sendno : ' . $result->sendno . $br;
     echo 'msg_id : ' .$result->msg_id . $br;
-    echo $br . '---------' . $br;
-} else {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
-    echo 'code : ' . $result->error->code . $br;
-    echo 'message : ' . $result->error->message . $br;
+    echo 'Response JSON : ' . $result->json . $br;
+} catch (APIRequestException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'Http Code : ' . $e->httpCode . $br;
+    echo 'code : ' . $e->code . $br;
+    echo 'message : ' . $e->message . $br;
+    echo 'Response JSON : ' . $e->json . $br;
+    echo 'rateLimitLimit : ' . $e->rateLimitLimit . $br;
+    echo 'rateLimitRemaining : ' . $e->rateLimitRemaining . $br;
+    echo 'rateLimitReset : ' . $e->rateLimitReset . $br;
+} catch (APIConnectionException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'message' . $e->getMessage() . $br;
 }
 
+echo $br . '-------------' . $br;
 
 //full push
+try {
+    $result = $client->push()
+        ->setPlatform(M\platform('ios', 'android'))
+        ->setAudience(M\audience(M\tag(['555','666']), M\alias(['555', '666'])))
+        ->setNotification(M\notification('Hi, JPush', M\android('Hi, android'), M\ios('Hi, ios', 'happy', 1, true)))
+        ->setMessage(M\message('msg content', null, null, array('key'=>'value')))
+        ->setOptions(M\options(123456, null, null, false))
+        ->printJSON()
+        ->send();
 
-$result = $client->push()
-    ->setPlatform(M\platform('ios', 'android'))
-    ->setAudience(M\audience(M\tag(['555','666']), M\alias(['555', '666'])))
-    ->setNotification(M\notification('Hi, JPush', M\android('Hi, android'), M\ios('Hi, ios', 'happy', 1, true)))
-    ->setMessage(M\message('msg content', null, null, array('key'=>'value')))
-    ->setOptions(M\options(123456, null, null, false))
-    ->printJSON()
-    ->send();
-
-if ($result->ok) {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
+    echo 'Push Success.' . $br;
     echo 'sendno : ' . $result->sendno . $br;
     echo 'msg_id : ' .$result->msg_id . $br;
-    echo $br . '---------' . $br;
-} else {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
-    echo 'code : ' . $result->error->code . $br;
-    echo 'message : ' . $result->error->message . $br;
+    echo 'Response JSON : ' . $result->json . $br;
+} catch (APIRequestException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'Http Code : ' . $e->httpCode . $br;
+    echo 'code : ' . $e->code . $br;
+    echo 'message : ' . $e->message . $br;
+    echo 'Response JSON : ' . $e->json . $br;
+    echo 'rateLimitLimit : ' . $e->rateLimitLimit . $br;
+    echo 'rateLimitRemaining : ' . $e->rateLimitRemaining . $br;
+    echo 'rateLimitReset : ' . $e->rateLimitReset . $br;
+} catch (APIConnectionException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'message' . $e->getMessage() . $br;
 }
+
+
+
+echo $br . '-------------' . $br;
 
 
 //fail push
-$result = $client->push()
-    ->setPlatform(M\all)
-    ->setAudience(M\all)
-    ->setNotification(M\notification('Hi, JPush'))
-    ->setAudience(M\audience(['no one']))
-    ->printJSON()
-    ->send();
+try {
+    $result = $client->push()
+        ->setPlatform(M\all)
+        ->setAudience(M\all)
+        ->setNotification(M\notification('Hi, JPush'))
+        ->setAudience(M\audience(['no one']))
+        ->printJSON()
+        ->send();
 
-if ($result->ok) {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
+    echo 'Push Success.' . $br;
     echo 'sendno : ' . $result->sendno . $br;
     echo 'msg_id : ' .$result->msg_id . $br;
-    echo $br . '---------' . $br;
-} else {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
-    echo 'code : ' . $result->error->code . $br;
-    echo 'message : ' . $result->error->message . $br;
+    echo 'Response JSON : ' . $result->json . $br;
+} catch (APIRequestException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'Http Code : ' . $e->httpCode . $br;
+    echo 'code : ' . $e->code . $br;
+    echo 'message : ' . $e->message . $br;
+    echo 'Response JSON : ' . $e->json . $br;
+    echo 'rateLimitLimit : ' . $e->rateLimitLimit . $br;
+    echo 'rateLimitRemaining : ' . $e->rateLimitRemaining . $br;
+    echo 'rateLimitReset : ' . $e->rateLimitReset . $br;
+} catch (APIConnectionException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'message' . $e->getMessage() . $br;
 }
--- examples/ReportExample.php
+++ examples/ReportExample.php
@@ -4,8 +4,12 @@ require_once 'vendor/autoload.php';
 use JPush\Model as M;
 use JPush\JPushClient;
 use JPush\JPushLog;
+
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+
+use JPush\Exception\APIConnectionException;
+use JPush\Exception\APIRequestException;
 
 $br = '<br/>';
 $spilt = ' - ';
@@ -16,21 +20,28 @@ $app_key='47a3ddda34b2602fa9e17c01';
 JPushLog::setLogHandlers(array(new StreamHandler('jpush.log', Logger::DEBUG)));
 $client = new JPushClient($app_key, $master_secret);
 
-$msg_ids = '1931816610,1466786990,1931499836';
-$result = $client->report($msg_ids);
 
-if ($result->ok) {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
+try {
+    $msg_ids = '1931816610,1466786990,1931499836';
+    $result = $client->report($msg_ids);
     foreach($result->received_list as  $received) {
         echo '---------' . $br;
         echo 'msg_id : ' . $received->msg_id . $br;
         echo 'android_received : ' .  $received->android_received . $br;
         echo 'ios_apns_sent : ' .  $received->ios_apns_sent . $br;
     }
-} else {
-    echo 'ok : ' . ($result->ok ? 'true' : 'false') . $br;
-    echo 'code : ' . $result->error->code . $br;
-    echo 'message : ' . $result->error->message . $br;
+} catch (APIRequestException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'Http Code : ' . $e->httpCode . $br;
+    echo 'code : ' . $e->code . $br;
+    echo 'message : ' . $e->message . $br;
+    echo 'Response JSON : ' . $e->json . $br;
+    echo 'rateLimitLimit : ' . $e->rateLimitLimit . $br;
+    echo 'rateLimitRemaining : ' . $e->rateLimitRemaining . $br;
+    echo 'rateLimitReset : ' . $e->rateLimitReset . $br;
+} catch (APIConnectionException $e) {
+    echo 'Push Fail.' . $br;
+    echo 'message' . $e->getMessage() . $br;
 }
 
 
