90d3cee6b255410de01f7ab659d286d5d5d0e255
xiezefan <xiezefan@126.com>
2015-12-18 14:49:12 +0800

    添加request请求类

--- core/JPush/JPush.php
+++ core/JPush/JPush.php
@@ -19,13 +19,15 @@ class JPush {
     const READ_TIMEOUT = 30;
     const DEFAULT_MAX_RETRY_TIMES = 3;
 
+    const HTTP_GET = 0x10000;
+    const HTTP_POST = 0x20000;
+
     public $appKey;
     public $masterSecret;
     public $retryTimes;
 
 
-    public function __construct($appKey, $masterSecret, $retryTimes=self::DEFAULT_MAX_RETRY_TIMES)
-    {
+    public function __construct($appKey, $masterSecret, $retryTimes=self::DEFAULT_MAX_RETRY_TIMES) {
         if (is_null($appKey) || is_null($masterSecret)) {
             throw new InvalidArgumentException("appKey and masterSecret must be set.");
         }
@@ -38,6 +40,78 @@ class JPush {
         $this->retryTimes = $retryTimes;
     }
 
+    public function push() {
+        return new PushPayload($this);
+    }
+
+
+
+
+
+    /**
+     * 发送HTTP请求
+     * @param $url String 请求的URL
+     * @param $method Int 请求的方法
+     * @param null $body String POST请求的Body
+     * @return array
+     */
+    public function _request($url, $method, $body=null) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        // 设置User-Agent
+        curl_setopt($ch, CURLOPT_USERAGENT, self::USER_AGENT);
+        // 连接建立最长耗时
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, self::CONNECT_TIMEOUT);
+        // 请求最长耗时
+        curl_setopt($ch, CURLOPT_TIMEOUT, self::READ_TIMEOUT);
+        // 设置SSL版本
+        curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1);
+        curl_setopt($ch, CURLOPT_SSL_CIPHER_LIST, 'TLSv1');
+        // 设置Basic认证
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->appKey . ":" . $this->masterSecret);
+        // 设置Post参数
+        if ($method === self::HTTP_POST) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        }
+        // 设置headers
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Connection: Keep-Alive'
+        ));
+
+        // 执行请求
+        $output = curl_exec($ch);
+        // 解析Response
+        $response = array();
+        $errorCode = curl_errno($ch);
+        if ($errorCode) {
+            $response['error'] = $errorCode;
+            $response['error_desc'] = curl_error($ch);
+        } else {
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            $header_text = substr($output, 0, $header_size);
+            $body = substr($output, $header_size);
+            $headers = array();
+            foreach (explode("\r\n", $header_text) as $i => $line) {
+                if ($i === 0) {
+                    $headers['http_code'] = $line;
+                } else {
+                    list ($key, $value) = explode(': ', $line);
+                    $headers[$key] = $value;
+                }
+            }
+            $response['headers'] = $headers;
+            $response['body'] = $body;
+            $response['http_code'] = $httpCode;
+        }
+        curl_close($ch);
+        return $response;
+    }
 
 
 
