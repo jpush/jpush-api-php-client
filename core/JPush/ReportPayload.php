a18e4884c569c5a5d433b556a15eb8cca1285862
xiezefan <xiezefan@126.com>
2015-12-21 15:07:17 +0800

    完成User Report Api

--- core/JPush/ReportPayload.php
+++ core/JPush/ReportPayload.php
@@ -1,6 +1,8 @@
 <?php
 
 class ReportPayload {
+    private static $EFFECTIVE_TIME_UNIT = array('HOUR', 'DAY', 'MONTH');
+
     const REPORT_URL = 'https://report.jpush.cn/v3/received';
     const MESSAGES_URL = 'https://report.jpush.cn/v3/messages';
     const USERS_URL = 'https://report.jpush.cn/v3/users';
@@ -64,7 +66,13 @@ class ReportPayload {
     }
 
     public function getUsers($time_unit, $start, $duration) {
+        $time_unit = strtoupper($time_unit);
+        if (!in_array($time_unit, self::$EFFECTIVE_TIME_UNIT)) {
+            throw new InvalidArgumentException('Invalid time unit');
+        }
 
+        $url = ReportPayload::USERS_URL . '?time_unit=' . $time_unit . '&start=' . $start . '&duration=' . $duration;
+        return $this->__request($url);
     }
 
     private function __request($url) {
