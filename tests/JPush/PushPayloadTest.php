b4e3f2f2df41bfe389335375a07325c5a4463db8
Helperhaps <helperhaps@gmail.com>
2016-07-14 17:39:40 +0800

    add some tests

--- tests/JPush/PushPayloadTest.php
+++ tests/JPush/PushPayloadTest.php
@@ -7,18 +7,115 @@ class PushPayloadTest extends \PHPUnit_Framework_TestCase {
         global $client;
         $this->client = $client;
         $this->pusher = $client->push();
+        $this->payload = $this->pusher
+                              ->setPlatform('all')
+                              ->setNotificationAlert('Hello JPush');
     }
 
     public function testSimplePushToAll() {
-        $response = $this->pusher->setPlatform('all')
-                         ->addAllAudience()
-                         ->setNotificationAlert('Hello World')
-                         ->send();
+        $payload = $this->payload;
+        $result = $payload->build();
+
+        $this->assertTrue(is_array($result));
+        $this->assertEquals(4, count($result));
+        $this->assertArrayHasKey('platform', $result);
+        $this->assertArrayHasKey('audience', $result);
+        $this->assertArrayHasKey('notification', $result);
+        $this->assertArrayHasKey('options', $result);
+
+        $response = $payload->send();
         $this->assertEquals('200', $response['http_code']);
         $body = $response['body'];
         $this->assertTrue(is_array($body));
         $this->assertEquals(2, count($body));
         $this->assertArrayHasKey('sendno', $body);
         $this->assertArrayHasKey('msg_id', $body);
+    }
+
+    public function testSetPlatform() {
+        $payload = $this->payload->addAllAudience();
+
+        $result1 = $payload->build();
+        $this->assertEquals('all', $result1['platform']);
+
+        $result2 = $payload->setPlatform('ios')->build();
+        $this->assertTrue(is_array($result2['platform']));
+    }
+
+    public function testSetAudience() {
+        $result = $this->payload->addAllAudience()->build();
+        $this->assertEquals('all', $result['audience']);
+    }
+
+    public function testAddTag() {
+        $payload = $this->payload;
+        $result = $payload->addTag('hello')->build();
+        $audience = $result['audience'];
+        $this->assertTrue(is_array($audience['tag']));
+        $this->assertEquals(1, count($audience['tag']));
+
+        $result = $payload->addTag(array('jpush', 'jiguang'))->build();
+        $this->assertEquals(3, count($result['audience']['tag']));
+    }
+    public function testAddTag2() {
+        $payload = $this->payload;
+        $result = $payload->addTagAnd(array('jpush', 'jiguang'))->build();
+        $audience = $result['audience'];
+        $this->assertTrue(is_array($audience['tag_and']));
+        $this->assertEquals(2, count($audience['tag_and']));
+
+        $result = $payload->addTagAnd('hello')->build();
+        $this->assertEquals(3, count($result['audience']['tag_and']));
+    }
+
+    public function testAddTagAnd1() {
+        $payload = $this->payload;
+        $result = $payload->addTagAnd('hello')->build();
+        $audience = $result['audience'];
+        $this->assertTrue(is_array($audience['tag_and']));
+        $this->assertEquals(1, count($audience['tag_and']));
+
+        $result = $payload->addTagAnd(array('jpush', 'jiguang'))->build();
+        $this->assertEquals(3, count($result['audience']['tag_and']));
+    }
+    public function testAddTagAnd2() {
+        $payload = $this->payload;
+        $result = $payload->addTagAnd(array('jpush', 'jiguang'))->build();
+        $audience = $result['audience'];
+        $this->assertTrue(is_array($audience['tag_and']));
+        $this->assertEquals(2, count($audience['tag_and']));
+
+        $result = $payload->addTagAnd('hello')->build();
+        $this->assertEquals(3, count($result['audience']['tag_and']));
+    }
+
+    public function testAddRegistrationId1() {
+        $payload = $this->payload;
+        $result = $payload->addRegistrationId('hello')->build();
+        $audience = $result['audience'];
+        $this->assertTrue(is_array($audience['registration_id']));
+        $this->assertEquals(1, count($audience['registration_id']));
+
+        $result = $payload->addRegistrationId(array('jpush', 'jiguang'))->build();
+        $this->assertEquals(3, count($result['audience']['registration_id']));
+    }
+    public function testAddRegistrationId2() {
+        $payload = $this->payload;
+        $result = $payload->addRegistrationId(array('jpush', 'jiguang'))->build();
+        $audience = $result['audience'];
+        $this->assertTrue(is_array($audience['registration_id']));
+        $this->assertEquals(2, count($audience['registration_id']));
+
+        $result = $payload->addRegistrationId('hello')->build();
+        $this->assertEquals(3, count($result['audience']['registration_id']));
+    }
+
+    public function testSetNotificationAlert() {
+        $result = $this->payload->addAllAudience()->build();
+        $notification = $result['notification'];
+
+        $this->assertTrue(is_array($notification));
+        $this->assertEquals(1, count($notification));
+        $this->assertEquals('Hello JPush', $result['notification']['alert']);
     }
 }
